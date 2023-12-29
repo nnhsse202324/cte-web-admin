0ac5fbe53265c18a739ff3d880fdebfe9f9e925a
Geoff Schmit <gcschmit@gmail.com>
2023-12-29 13:08:38 -0600

    update based on latest Node app template

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,20 +1,18 @@
-const mongoose = require('mongoose');
+/**
+ * The exported function establishes a connection to the specified MongoDB database via the Mongoose module.
+ */
+
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try{
-        // mongodb connection string
-        const con = await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useFindAndModify: false,
-            useCreateIndex: true
-        })
+    const con = await mongoose.connect(process.env.MONGO_URI);
 
         console.log(`MongoDB connected : ${con.connection.host}`);
     }catch(err){
         console.log(err);
         process.exit(1);
     }
-}
+};
 
-module.exports = connectDB
+module.exports = connectDB;
