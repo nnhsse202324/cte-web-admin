e35e7b8cf9c2e52d63dedb4b770f48e3a1b21845
panduh-s <ybu2006@gmail.com>
2024-01-19 09:34:23 -0600

    Started Coding

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,53 +1,51 @@
-// !!! split into 2 files - courses.js and cerrtficates.js
+// !!! split into 2 files - courses.js and certficates.js
 
-async function submit(){
-    var courses = Array.from(document.querySelectorAll(".course_checkbox"));
-    var courseNames = [];
+async function submit() {
+  var courses = Array.from(document.querySelectorAll(".course_checkbox"));
+  var courseNames = [];
 
-    for(course of courses)
-    {
-        if(course.checked) {
-            courseNames.push(course.id)
-        }
+  for (course of courses) {
+    if (course.checked) {
+      courseNames.push(course.id);
     }
+  }
 
-    const response = await fetch("/courses", {
-        method: 'POST', // *GET, POST, PUT, DELETE, etc.
-        headers: {
-        'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(courseNames) // body data type must match "Content-Type" header
-    });
-    if(response.ok) {
-        window.location = "/certificates";
-    }
-    else {
-        console.log("error");
-    }
+  const response = await fetch("/courses", {
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(courseNames), // body data type must match "Content-Type" header
+  });
+  if (response.ok) {
+    window.location = "/certificates";
+  } else {
+    console.log("error");
+  }
 }
 
-async function submitCertificates(){
-    var certificates = Array.from(document.querySelectorAll(".certificate_checkbox"));
-    var certificateNames = [];
+async function submitCertificates() {
+  var certificates = Array.from(
+    document.querySelectorAll(".certificate_checkbox")
+  );
+  var certificateNames = [];
 
-    for(certificate of certificates)
-    {
-        if(certificate.checked) {
-            certificateNames.push(certificate.id)
-        }
+  for (certificate of certificates) {
+    if (certificate.checked) {
+      certificateNames.push(certificate.id);
     }
+  }
 
-    const response = await fetch("/certificates", {
-        method: 'POST', // *GET, POST, PUT, DELETE, etc.
-        headers: {
-        'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(certificateNames) // body data type must match "Content-Type" header
-    });
-    if(response.ok) {
-        window.location = "/confirmation";
-    }
-    else {
-        console.log("error");
-    }
+  const response = await fetch("/certificates", {
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(certificateNames), // body data type must match "Content-Type" header
+  });
+  if (response.ok) {
+    window.location = "/confirmation";
+  } else {
+    console.log("error");
+  }
 }
