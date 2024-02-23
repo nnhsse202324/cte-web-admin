c6f3c10ca0b308582a377c58f51319057ec2b43f
Galeilian <wddyhxdyx@126.com>
2024-02-23 09:00:19 -0600

    Donwload NOT EMPTY csv

--- assets/js/index.js
+++ assets/js/index.js
@@ -50,7 +50,8 @@ async function submitCertificates() {
   }
 }
 
-function downloadEmptyCSV() {
+/**
+ * function downloadEmptyCSV() {
   const csvContent = "data:text/csv;charset=utf-8,\n";
 
   const encodedUri = encodeURI(csvContent);
@@ -61,3 +62,5 @@ function downloadEmptyCSV() {
   link.click();
   document.body.removeChild(link);
 }
+
+ */
